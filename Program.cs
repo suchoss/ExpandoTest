0fe54a14e956c3d52bb9e710b6f5dce8d6436b7e
Suchoss <suchoss@gmail.com>
2022-04-06 10:38:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+var expConverter = new Newtonsoft.Json.Converters.ExpandoObjectConverter();
+
+var json = File.ReadAllText("test.json");
+
+var dynamic = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(json, expConverter);
+
+var existingProp = dynamic.Url;
+var existingPropDifferentCase = dynamic.url;
+var nonExistingProp = dynamic.dbCreated;
